470b1b1c5b901b46dbd188a6592784fc56e9bfe0
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-08-14 16:26:15 +0700

    - add Validation when create new product

--- src/main/java/com/t3h/module2/demo/Controller/ProductController.java
+++ src/main/java/com/t3h/module2/demo/Controller/ProductController.java
@@ -1,12 +1,20 @@
 package com.t3h.module2.demo.Controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import com.t3h.module2.demo.Model.Product;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -20,9 +28,14 @@ public class ProductController {
         @RequestParam String category,
         @RequestParam Double price){
 
-        Product product = new Product(name, category, price);
+        Product product = new Product("test", name, category, "test", price, new ArrayList<>());
 
         return product;
+    }
+
+    @PostMapping("/api/product")
+    public ResponseEntity<@Valid Product> createNewProduct(@Valid @RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
 }
--- src/main/java/com/t3h/module2/demo/Model/Product.java
+++ src/main/java/com/t3h/module2/demo/Model/Product.java
@@ -1,5 +1,12 @@
 package com.t3h.module2.demo.Model;
 
+import java.util.List;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +16,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor      // Generates no-args constructor (needed for Jackson)
 
 public class Product {
+    @Size(min = 5, max = 50, message = "Product ID must be between 5 and 50 characters")
+    String productID;
+
+    @Size(min = 50, message = "Name must have at least 50 characters")
     String name;
+
+    @NotNull(message = "Category cannot be null")
     String category;
+    
+    @NotBlank(message = "Description cannot be blank")
+    String description;
+
+    @DecimalMin(value = "0.1", inclusive = true, message = "Price must be at least 0.1")
+    @DecimalMax(value = "100.0", inclusive = true, message = "Price must be at most 100")
     Double price;
+    
+    @NotNull(message = "Tags cannot be null")
+    @Size(min = 1, message = "At least one tag is required")
+    List<String> tags;
 }
