9f2a5efd3c75ff4461858ba5abd2d6ac30ccba43
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-08-14 16:59:42 +0700

    - upload files API

--- src/main/java/com/t3h/module2/demo/Controller/Home.java
+++ src/main/java/com/t3h/module2/demo/Controller/Home.java
@@ -1,17 +1,26 @@
 package com.t3h.module2.demo.Controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @RestController
 public class Home {
+    private static final String STATIC_DIR = "src/main/resources/static/";
 
     @Autowired
     private SpringTemplateEngine templateEngine; // Inject Thymeleaf's template engine
@@ -62,6 +71,35 @@ public class Home {
     public String display3_3() {
         Context context = new Context();
         return templateEngine.process("unit_javascript", context);
+    }
+
+    @PostMapping(value = "/api/images")
+    public ResponseEntity<String> uploadImages(@RequestParam("files") List<MultipartFile> files) {
+        // Validate
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("One of the files is empty: " + file.getOriginalFilename());
+            }
+            String contentType = file.getContentType();
+            if (contentType == null || !contentType.startsWith("image/")) {
+                return new ResponseEntity<>("One of file is invalid, filename: " + file.getOriginalFilename(), HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        // Save files
+        try {
+            for (MultipartFile file : files) {
+                Path targetPath = Paths.get(STATIC_DIR + file.getOriginalFilename())
+                        .normalize().toAbsolutePath();
+                Files.createDirectories(targetPath.getParent());
+                file.transferTo(targetPath.toFile());
+            }
+        } catch (IOException e) {
+            return new ResponseEntity<>("Files cannot be uploaded", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>("Files are received successfully", HttpStatus.OK);
     }
 }
 
